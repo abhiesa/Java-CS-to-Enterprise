f2325c8647147d10d93e25439edf5025b4c2990f
Abhishek Prashant Pandey <abhiesa@users.noreply.github.com>
2017-05-30 17:18:15 +0530

    Update HelloWorldTest.java

--- src/test/java/com/abhiesa/blog/HelloWorldTest.java
+++ src/test/java/com/abhiesa/blog/HelloWorldTest.java
@@ -1,15 +1,21 @@
 package com.abhiesa.blog;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
 public class HelloWorldTest {
+
   private HelloWorld subject;
+
   @Before
   public void setup() {
     subject = new HelloWorld();
   }
+
   @Test
   public void testGetMessage() {
     assertEquals("Hello World!", subject.getMessage(false));
   }
+
 }
